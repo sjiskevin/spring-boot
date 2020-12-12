fc534bca296b63070b1b29e63a8a2193e0871974
sjkevinshen <1017069002@qq.com>
2020-12-12 20:26:53 +0800

    1认识springboot-web、spring-boot-actuator、spring-boot-security

---
+++ spring-boot-start/src/main/java/org/kevinshen/springboot/start/security/ActuatorSecurityConfig.java
@@ -0,0 +1,20 @@
+package org.kevinshen.springboot.start.security;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.context.ShutdownEndpoint;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatcher(EndpointRequest.toAnyEndpoint()).authorizeRequests()
+                .anyRequest().hasRole("ENDPOINT_ADMIN")
+                .and()
+                .httpBasic();
+    }
+}
